b9d9bf30493446e4966b5a0731cf650c5751eb74
Raman <khanahmadi.raman@gmail.com>
2023-11-22 07:27:27 -0800

    complete project

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CustomersComponent } from './customers.component';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
 
-const routes: Routes = [{ path: '', component: CustomersComponent }];
+const routes: Routes = [
+  { path: '', component: CustomersComponent },
+  { path: 'add-customer', component: AddCustomerComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CustomersRoutingModule { }
+export class CustomersRoutingModule {}
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomersComponent } from './customers.component';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
 
 
 @NgModule({
   declarations: [
-    CustomersComponent
+    CustomersComponent,
+    AddCustomerComponent
   ],
   imports: [
     CommonModule,
